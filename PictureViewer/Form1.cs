705c37547bcca83a9c7995ed4ad36d752d3d2757
Vladyslav <vlad@exemple.com>
2016-09-09 16:29:26 +0300

    Completed step 6 of 11

--- PictureViewer/Form1.cs
+++ PictureViewer/Form1.cs
@@ -21,5 +21,30 @@ namespace PictureViewer
         {
 
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
